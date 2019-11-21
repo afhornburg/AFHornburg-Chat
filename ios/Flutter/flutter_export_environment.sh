e97183011358bd84aec6a53ba8baf386833ddef9
augusto015 <augusto.hornburg@gmail.com>
2019-11-21 15:02:14 -0300

    ada

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Augusto Hornburg\Downloads\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Augusto Hornburg\Downloads\flutter-chat-demo-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Augusto Hornburg\Downloads\flutter\bin\cache\artifacts\engine\ios"
